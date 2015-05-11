1ffceb1532195825f7a3f25922371de888ed0eae
Pandada8 <pandada8@gmail.com>
2015-05-11 17:36:44 +0800

    pep8 and static file handling for slide

--- focus/views/slide/__init__.py
+++ focus/views/slide/__init__.py
@@ -1,16 +1,17 @@
-from flask import Blueprint
-from focus import app
-from functools import wraps
-NAME = "slide"
+from flask import Blueprint, render_template
+from focus import app, mongodb, send_from_directory
 
+NAME = "slide"
 main = Blueprint(NAME, __name__, template_folder="templates")
 app.register_blueprint(main)
 main.TITLE = "多图全屏轮播"
 actions = {}
 
-def register_action(name): 
+
+def register_action(name):
     def wrapper(func):
         actions[name] = func
+
         def do(*args, **kwargs):
             return func(*args, **kwargs)
 
@@ -21,7 +22,6 @@ def register_action(name):
 
 
 def init():
-
     """
     put the init task here, like create the folder or init the data
     This is a simple example, so nothing needed.
@@ -32,5 +32,16 @@ def init():
 
 @main.route("/admin/project/<proj(slide):project>/change_pics")
 @register_action('图片管理')
-def pictures_management(project):
-    return "Change Pic of " + p
+def slide_pictures_management(project):
+    return "Change Pic of " + project
+
+
+@main.route("/p/<proj(slide):project>/")
+def slide_index(project):
+    pics = mongodb[project].find("pictures")
+    return render_template("index.html", pics=pics)
+
+
+@main.route("/p/<proj(slide):project>/<path:static>")
+def slide_static(project, static):
+    return send_from_directory('static', static)
